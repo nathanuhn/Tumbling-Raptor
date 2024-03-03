f2d7b2ae3abdf843879447e7053f809d3d5f6cc8
nathanuhn <nathan.uhn@gmail.com>
2024-03-03 17:13:34 -0600

    I did jumping stuff

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,9 +2,25 @@ extends CharacterBody2D
 
 @export var gravity = 1
 
+@export var jump_power = 1
+
+var active = true
+
+var jump_remaining = 50
+
+var was_jumping = false
+
 func _ready():
 	print("hello world")
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
+	
+	if active:
+		
+		if Input.is_action_just_pressed("jump") and jump_remaining > 0:
+			jump_remaining -= 1
+			was_jumping = true
+			velocity.y = -jump_power
+	
 	move_and_slide()
